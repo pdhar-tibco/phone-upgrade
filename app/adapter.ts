5509149c7922a02f17388819343cee4ab3c9eff4
Pranab Dhar <pdhar@tibco.com>
2016-11-15 09:20:29 -0800

    Add Adapter

---
+++ app/adapter.ts
@@ -0,0 +1,5 @@
+import { AppModule } from './app.module';
+import {forwardRef } from '@angular/core';
+import { UpgradeAdapter } from '@angular/upgrade';
+
+export const upgradeAdapter = new UpgradeAdapter(forwardRef(()=>AppModule));
